3d98317327a9a0ecb4e8890ea8cdd08b5cbc7dc1
FiNiTe-Weeb <101021772+FiNiTe-Weeb@users.noreply.github.com>
2024-01-15 00:36:40 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,204 @@
+import welcome from "welcome.html";
+import pointercrateLoad from "pointercrateLoad.js";
+
+/**
+ * @typedef {Object} Env
+ */
+
+const allowedLists=["test","pointercrate","insaneDemonList","lowRefreshRateList","aredl","challengeList"];
+const allowedEvents=["initLoad","apiGetPlayer","apiSearchPlayer"];
+const allowedTargets=["score","leaderboard"];
+
+export default {
+	/**
+	 * @param {Request} request
+	 * @param {Env} env
+	 * @param {ExecutionContext} ctx
+	 * @returns {Promise<Response>}
+	 */
+	async fetchDefault(request, env, ctx) {
+		const url = new URL(request.url);
+		console.log(`Hello ${navigator.userAgent} at path ${url.pathname}!`);
+
+		if (url.pathname === "/api") {
+			// You could also call a third party API here
+			const data = await import("./data.js");
+			return Response.json(data);
+		}
+		return new Response(welcome, {
+			headers: {
+				"content-type": "text/html",
+			},
+		});
+	},
+	/**
+	 * @param {Request} request
+	 * @param {Env} env
+	 * @param {ExecutionContext} ctx
+	 * @returns {Promise<Response>}
+	 */
+	async fetch(request, env, ctx) {
+		const url = new URL(request.url);
+		let path=url.pathname.substring(1);
+		//console.log(`Hello ${navigator.userAgent} at path ${url.pathname}!`);
+		let splitPath=path.split('/');
+
+		console.log("Method: "+request.method+", path: "+path+", splitPath: "+splitPath);
+
+		switch(splitPath[0]){
+			case "api":
+				return await handleApi(request,env,ctx,splitPath.slice(1));
+			case "rankcache":
+				return await handleRankCache(request,env,ctx,splitPath.slice(1));
+		}
+
+		return badReq();
+	},
+	/**
+	 * @param {ScheduledEvent} event
+	 * @param {Env} env
+	 * @param {ExecutionContext} ctx
+	 */
+	async scheduled(event, env, ctx) {
+		console.log(event.scheduledTime);
+		let leaderboard=await pointercrateLoad.pageLoader(0,100);
+		let pointsArr=[];
+		for(let i=0;i<leaderboard.length;i++){
+			pointsArr.push(leaderboard[i].score);
+		}
+		console.log(pointsArr);
+		await env.rankingsCache.put("pointercrate-leaderboard",JSON.stringify(leaderboard));
+		await env.rankingsCache.put("pointercrate-score",JSON.stringify(pointsArr));
+	},
+};
+
+/**
+ * @param {Request} request
+ * @param {Env} env
+ * @param {ExecutionContext} ctx
+ * @param {Array} splitPath
+ * @returns {Promise<Response>}
+ */
+async function handleApi(request,env,ctx,splitPath){
+	if(request.headers.get("content-type")!="application/json"){
+		return badReq("Only content-type:application/json requests are supported");
+	}
+	if(request.method!="POST"){
+		return badReq("POST Request pls!!!");
+	}
+	try{
+		let requestBody=await request.json();
+		console.log(requestBody);
+
+		let list=requestBody.list;
+		let event=requestBody.event;
+		if(list==undefined){return badReq("Missing list")}
+		if(event==undefined){return badReq("Missing event")}
+		if(allowedLists.indexOf(list)<0){return badReq("Invalid list")}
+		if(allowedEvents.indexOf(event)<0){return badReq("Invalid event")}
+
+
+		let listData=JSON.parse(await env.usages.get(list));
+		if(listData==null){
+			listData={
+				event:{totals:{}}
+			}
+		}
+		if(listData.event==undefined){listData.event={};}
+		if(listData.event.totals==undefined){listData.event.totals={};}
+		if(listData.event.totals[event]==undefined){listData.event.totals[event]=0;}
+		listData.event.totals[event]++;
+		await env.usages.put(list,JSON.stringify(listData));
+
+
+		return new Response("{\"data\":"+JSON.stringify({
+			...requestBody,
+			count:listData.event.totals[event]
+		})+",\"status\":\"201\"}", {
+			headers: {
+				"content-type": "application/json",
+			},
+			status:201
+		});
+	}catch(exception){
+		console.log(exception.message);
+		return badReq(exception.message);
+	}
+	return badReq("Bad Request to API");
+
+}
+
+/**
+ * @param {Request} request
+ * @param {Env} env
+ * @param {ExecutionContext} ctx
+ * @param {Array} splitPath
+ * @returns {Promise<Response>}
+ */
+async function handleRankCache(request,env,ctx,splitPath){
+	return badReq("Temporarily disabled endpoint");
+	let list=splitPath[0];
+	let target=splitPath[1];
+	if(list==undefined||list==""){
+		return badReq("Missing path segment (e.g: /rankcache/pointercrate/score)");
+	}
+	if(allowedLists.indexOf(list)<0){
+		return badReq("Invalid list");
+	}
+	if(allowedTargets.indexOf(target)<0){
+		return badReq("Invalid target, allowed targets: "+allowedTargets.join(', '));
+	}
+	if(target=="leaderboard"){
+		return badReq("Temporarily disabled target");
+	}
+	let rankCache=await env.rankingsCache.get(list+"-"+target);
+	if(rankCache==null){
+		let responseDat={
+			message:"There is currently no rank cache for this list",
+			status:404
+		};
+		return new Response(JSON.stringify(responseDat),{
+			headers: {"content-type": "application/json",},
+			status:404
+		});
+	}
+	return new Response(rankCache,{
+		headers: {"content-type": "application/json",},
+		status:200
+	});
+}
+
+/**
+ * @param {String} msgOverride
+ * @returns {Response}
+ */
+function badReq(msgOverride="Nuh uh! Bad Request!!"){
+	let responseDat={
+		message:msgOverride,
+		status:400
+	};
+	return new Response(JSON.stringify(responseDat), {
+		headers: {"content-type": "application/json",},
+		status:400
+	});
+}
+
+/**
+ * @param {Request} request
+ */
+function logHeaders(request){
+	for(let entry of request.headers.entries()){
+		console.log(entry[0]+": "+request.headers.get(entry[0]));
+	}
+}
+
+/*
+send:
+fetch("https://workers-playground-hidden-frost-1d0a.finite-10-07.workers.dev/api",{
+  method:"POST",
+	body:"{\"list\":\"test\",\"event\":\"initLoad\"}",
+	headers:new Headers({"content-type":"application/json"})
+}).then(function(resp){return resp.json();}).then(console.log);
+*/
+
+//https://pointercrate.com/api/v1/players/ranking/?limit=100&after=100
---
+++ pointercrateLoad.js
@@ -0,0 +1,43 @@
+/**
+ * NOTES:
+ * 1. leaderboard can be inaccurate when players have same score across page borders, same player may be shown a 2nd time instead of another player
+ * 2. score/leaderboard cache will end when they hit 0 score, but only stops at loading next page, so u might see a few trailing 0 score-entries
+ */
+export default{
+    pageLoader(afterPage=0,pageLength=100,limit=Infinity){
+    let url="https://pointercrate.com/api/v1/players/ranking/";
+    console.log("loading page "+(afterPage+1));
+    let thisRef=this;
+    let fetchPromise=new Promise(function(res,rej){//todo: handle error idk
+        fetch(url+"?limit="+Math.min(pageLength,limit)+"&after="+(afterPage*pageLength)).then(function(resp){
+            if(!resp.ok){
+                return Promise.reject(resp);
+            }
+            return resp.json();
+        }).then(function(data){
+            if(data.length>=pageLength&&limit>pageLength&&data[data.length-1].score>0){//probably not last page
+                let prom=appendPromiseArr(data,thisRef.pageLoader(afterPage+1,pageLength,limit-pageLength));
+                res(prom);
+            }else{//last page
+                res(data);
+            }
+        }).catch(rej);
+    });
+    return fetchPromise;
+}
+}
+
+/**
+* Function I use to stack data from different pages
+* @param {Array} existingArr - array to append to.
+* @param {Promise<Array>} promiseArr - promise which is expected to resolve to an array, the elements of which will be appended to existingArr
+* @returns - The concatanated array
+*/
+function appendPromiseArr(existingArr,promiseArr){
+    let appendArrPromise=new Promise(function(res,rej){//todo: error handling
+        promiseArr.then(function(newArr){
+            res(existingArr.concat(newArr));
+        });
+    });
+    return appendArrPromise;
+}
